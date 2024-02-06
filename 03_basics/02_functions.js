b45fec4cf5751710a4b3049a923617f227b72a9b
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-06 15:49:34 +0000

    function basics

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,4 @@
+// When we don't know how parameters is needed because the arguments is given by the user, so user
+//  will add the more & more arguments then we don't how much paramenters is needed to declare
+// the solution is (...) rest operator. 
+// IMP: rest and spread operator are same [...], but we differtiate them on the use cases.
