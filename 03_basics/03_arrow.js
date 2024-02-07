c3eb4d7a47be2c0cf9c265d54ea8832e457acf3b
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-07 18:41:32 +0000

    Arrow in JS done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -90,14 +90,29 @@ const user = (num1, num2) => {
 console.log(user(10, 20));
 
 // OP: 30
+//Explicit return: when we use the return keyword, then it is explicit return type. above example.
+
+//   -- if we use the curly braces {}, there is need to write "return" type in code in above example 1 in arrow function.
 
 
-// Implicit return : There is no need to use {} in the function in the first line. means we assum there is node to typed to add the return type.
-// Example 2:
-//   -- if we use the curly braces, there is ned of return type in code.
+
+
+
+
+// Implicit return : There is no need to use {} in the function in the first line. means we assum there is automatically typed to add the return type.
+
+// Example 2:  
 const user = (num1, num2) => num1+num2
 console.log(user(10, 40));
 // OP:
+const user = (num1, num2) => (num1+num2)
+console.log(user(50, 50));
 //   -- if we are not use the curly braces, there is need of return type in code.
 
 
+
+
+const user = (num1, num2) => ({username: "Omkar"})
+console.log(user(3,4));
+
+// OP: { username: 'Omkar' }
