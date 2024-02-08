61a23e4f79dfad4f5a2c6c1f355cc34ae590e506
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-08 17:36:04 +0000

    functions

--- Practise/file_d5.js
+++ Practise/file_d5.js
@@ -1,4 +1,47 @@
 // Function
+
+// Example 1:
+function square(number){
+    return number*number
+}
+let check = square(10);
+console.log("Square of the number is: ", check);
+
+
+
+
+// Example 2:
+function fullName(fname, Lname){
+    return fname+Lname;
+}
+let result = fullName("Omkar ", "T")
+console.log(result);
+
+
+
+
+
+
+// Example 3:
+let greet = function(name){
+    console.log("Hello "+name+"!");
+}
+greet("Omkar")
+
+
+
+
+
+// Example 4:
+function example(){
+    let message = "This is my message."
+    console.log(message);
+}
+example()
+
+
+
+// Example 5:
 function keyboard(fname, lname){
     return fname+lname;
     // console.log(result);
