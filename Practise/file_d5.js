fb0f859b0ee09e6cde4a5b563da6af38f4376689
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-08 17:34:35 +0000

    function in JS

---
+++ Practise/file_d5.js
@@ -0,0 +1,8 @@
+// Function
+function keyboard(fname, lname){
+    return fname+lname;
+    // console.log(result);
+}
+let result = keyboard("Omkar ", "Tupere");
+console.log(result);
+// console.log('Omkar', 'Tupere');
