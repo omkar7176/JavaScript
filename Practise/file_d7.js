084030de9af8a41884229997c137f6043532ce6d
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-10 17:16:41 +0000

    Arrays

--- Practise/file_d7.js
+++ Practise/file_d7.js
@@ -11,3 +11,42 @@ fruits.push("Strawberry"); // adding the new elements
 console.log(fruits);
 
 console.log(fruits.length);
+
+
+
+let fruits = ["Apple", "Pineapple", "Grapes", "Mango"];
+console.log(fruits);
+
+let staticArray = [1,2,3,4,5,6,7];
+staticArray.push(9);   // Adding the elements
+staticArray.pop();    // remove the element
+console.log(staticArray);
+/* OP: [
+  1, 2, 3, 4,
+  5, 6, 7
+] */
+
+
+
+
+let sparseArray = []; // Array with undefined elements
+sparseArray[0] = 10;
+sparseArray[1] = 20;
+console.log(sparseArray);
+//OP: [ 10, 20 ]
+
+
+
+let intArray = new Int32Array(5); // predefined word Int32Array
+intArray[0] = 15;
+console.log(intArray);
+// OP: Int32Array(5) [ 15, 0, 0, 0, 0 ]
+
+
+
+
+function egfunction(){
+    console.log(arguments[0]);
+};
+egfunction(11);
+// OP: 11
---
+++ Practise/test.js
@@ -0,0 +1,5 @@
+let ArrayOriginal = [[1,2], [3,4,5], [6,7], [8,9,0]];
+
+let Arraycopy = [...ArrayOriginal];
+console.log(Arraycopy);
+
