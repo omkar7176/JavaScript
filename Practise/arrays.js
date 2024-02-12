e34728d960c1dbf360e4b8a8425e815d56510a88
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-12 18:07:46 +0000

    Added some methods

--- Practise/arrays.js
+++ Practise/arrays.js
@@ -7,3 +7,19 @@ console.log(lang[1]);
 let check = new Array(11, 12, 13, 14, 15, 16)
 console.log(check);
 console.log(check[3]);
+
+
+let newArray = new Array(51,52, 53, 54, 55)
+console.log(newArray);
+
+// newArray.push(60);
+// console.log(newArray);
+
+// newArray.pop();
+// console.log(newArray);
+
+newArray.unshift(100);
+console.log(newArray);
+
+newArray.shift();
+console.log(newArray);
