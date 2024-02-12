8cf2315df0091e94249a19341a2460578457ec42
omkar7176 <115610759+omkar7176@users.noreply.github.com>
2024-02-12 17:55:38 +0000

    Practise on array in JS

---
+++ Practise/arrays.js
@@ -0,0 +1,9 @@
+let lang = ["C++", "Java", "Python", "Go"]
+console.log(lang);
+console.log(typeof(lang));
+console.log(lang[1]);
+
+
+let check = new Array(11, 12, 13, 14, 15, 16)
+console.log(check);
+console.log(check[3]);
