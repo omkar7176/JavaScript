e09eeb902c26f2d5cf97fa8e36fb65fcb422d255
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2025-01-02 17:46:14 +0000

    Datatype Summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,32 @@
+// Primitive --> Call by value
+
+// 7 Types: String, Number, Boolean, Symbol, null, undefined, BigInt.
+
+// Non-Primitive --> Call by Reference
+
+// Array, Objects, Functions
+
+// 1. Array
+const lang = ["English", "Hindi", "Marathi", "Punjabi"];
+console.log(lang);
+console.log(typeof lang);
+
+
+
+// 2. Objects
+
+let Obj = {
+    Name: "Omkar",
+    Age:19
+}
+console.log(Obj);
+console.log(typeof Obj);
+
+
+// 3. Functions
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+myFunction();
+console.log(typeof myFunction());
